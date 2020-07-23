4537555aee8ecc6568e797bc4e7627de8dd5b975
D1uxe <artem.fx4y@gmail.com>
2020-07-23 17:53:04 +0300

    Добавил шаблоны классов для дальнейшего построения рабочего (основного) дерева

---
+++ ChildNodesPathDemo/Model/BaseNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChildNodesPathDemo.Model
+{
+    public abstract class BaseNode
+    {
+        private string _OrderNr;
+        public string OrderNr { get => _OrderNr; set => _OrderNr = value; }
+
+        private string _Description;
+        public string Description { get => _Description; set => _Description = value; }
+    }
+}
